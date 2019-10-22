a4380e7ced0a9cef6fd891b47a030bf4e6bf77f5
Johnny Hoang <johnny.hoang@yh.nackademin.se>
2019-10-22 13:30:25 +0200

    Fixed: Rewy Database.sql file is now up to date.

--- Rewy Database.sql
+++ Rewy Database.sql
@@ -1,22 +1,22 @@
 drop database if exists rewy;
 create database rewy;
 use rewy;
-
+	
 create table user(
-user_id int unsigned not null auto_increment primary key,
+id int unsigned not null auto_increment primary key,
 username varchar(45),
 password varchar(45),
-created timestamp DEFAULT CURRENT_TIMESTAMP
+created timestamp DEFAULT current_timestamp
 );
 
 create table admin(
-id int unsigned not null auto_increment primary key,
+	id int unsigned not null auto_increment primary key,
 user_id int unsigned not null,
-foreign key(user_id) references user(user_id)
+foreign key(user_id) references user(id)
 );
 
 create table movie(
-movie_id int unsigned not null auto_increment primary key,
+id int unsigned not null auto_increment primary key,
 name varchar(45),
 description varchar (300),
 rating double default 0
@@ -25,6 +25,9 @@ rating double default 0
 insert into user(username,password) values("Flakan","Flakan123");
 insert into user(username,password) values("Doooku","Doooku123");
 insert into user(username,password) values("FeedTown","FeedTown123");
+insert into user(username,password) values("1","Flakan123");
+insert into user(username,password) values("2","Doooku123");
+insert into user(username,password) values("3","FeedTown123");
 
 insert into admin(user_id) values(1);
 
